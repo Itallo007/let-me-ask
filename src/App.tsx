a56d7db367a607c73332b9bc25a734868ef91b66
Itallo007 <italosantos.oficial2000@gmail.com>
2021-06-22 12:35:56 -0300

    Move o código do contextode autenticação para arquivo separado
    toda a lógica e importações foram movidas do App.tsx para o arquivo
    /contexts/AuthContext.

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,19 @@
-import {createContext, useEffect, useState} from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 
-import { auth, firebase } from './services/firebase';
+import { AuthContextProvider } from './contexts/AuthContext';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
-type User = {
-  id: string,
-  name: string,
-  avatar: string
-}
-
-type AutoContextType = {
-  user: User | undefined,
-  signInWithGoogle: () => Promise<void>
-}
-
-export const AuthContext = createContext({} as AutoContextType);
-
 function App() {
-  const [user, setUser] = useState<User>();
-
-  useEffect(() => {
-    auth.onAuthStateChanged(user => {
-      if(user) {
-        const {displayName, photoURL, uid} = user;
-  
-        if(!displayName || !photoURL) {
-          throw new Error('Missing information from Google Account');
-        }
-  
-        setUser({
-          id: uid,
-          name: displayName,
-          avatar: photoURL
-        });
-      }
-    })
-  }, []);
-
-  async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const result = await auth.signInWithPopup(provider);
-
-    if(result.user) {
-      const {displayName, photoURL, uid} = result.user;
-
-      if(!displayName || !photoURL) {
-        throw new Error('Missing information from Google Account');
-      }
-
-      setUser({
-        id: uid,
-        name: displayName,
-        avatar: photoURL
-      });
-    }
-  }
-
   return (
     <BrowserRouter >
-      <AuthContext.Provider value={{user, signInWithGoogle}}>
+      <AuthContextProvider>
         {/* Rota para Home page ou tela de autenticação */}
         <Route path="/" exact component={Home}/>
 
         {/* Rota para tela de criar salas */}
         <Route path="/room/new" component={NewRoom}/>
-      </AuthContext.Provider>    
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
