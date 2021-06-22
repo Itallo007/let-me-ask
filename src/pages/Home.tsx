35c7e0fcd5f8606f6f8bc5a214fb8220effed7f6
Itallo007 <italosantos.oficial2000@gmail.com>
2021-06-22 12:39:08 -0300

    Cria o hook de autenticação
    as importações nos arquivos Home.tsx e NewRoom.tsx foram alteradas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
-import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -12,7 +11,7 @@ import '../styles/auth.scss';
 export function Home() {
 
   const history = useHistory();
-  const {user, signInWithGoogle} = useContext(AuthContext)
+  const {user, signInWithGoogle} = useAuth();
 
   async function handleCreateRoom() {
     if(!user) {
