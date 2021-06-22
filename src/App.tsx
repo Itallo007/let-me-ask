956a1fad9c76ef740c188968dda1977b5b5ce3c7
Itallo007 <italosantos.oficial2000@gmail.com>
2021-06-22 00:10:06 -0300

    Cria as rotas do app no arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter >
+    {/* Rota para Home page ou tela de autenticação */}
+    <Route path="/" exact component={Home}/>
+
+     {/* Rota para tela de criar salas */}
+     <Route path="/room/new" component={NewRoom}/>
+    
+    </BrowserRouter>
   );
 }
 
