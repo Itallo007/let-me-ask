705010e58e9cb6f95a820d0625762155f42c48ee
Itallo007 <italosantos.oficial2000@gmail.com>
2021-06-21 23:40:36 -0300

    Cria e estiliza a tela NewRoom
    a estrutura da tela foi copiada da Home e adaptada. As estilizações
    foram adicionadas no arquivo styles/auth.scss

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,35 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas e de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas de sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form >
+            <input
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+          <p>
+            Quer entrar em uma sala já existente? <a href="#">clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
